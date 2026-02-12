13d3bf635f691199320508ff38850265f20a9a4a
ULUGBEK399 <56226594+ULUGBEK399@users.noreply.github.com>
2026-02-12 08:50:36 +0500

    fixed ngnix django admin panel and changed url.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,10 +5,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-ADMIN_URL = os.environ.get("DJANGO_ADMIN_URL", "gc-management").strip("/")
-
 urlpatterns = [
-    path(f"{ADMIN_URL}/", admin.site.urls),
+    path("gc-management/", admin.site.urls),
     path("api/v1/", include("apps.api.urls")),
     path("i18n/", include("django.conf.urls.i18n")),  # Language switching
 ]
@@ -16,4 +14,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += [path("api/schema/", include("apps.api.schema_urls"))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
